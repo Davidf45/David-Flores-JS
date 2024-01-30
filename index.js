807bc2d1526e4ae432a30ff21a15183975c82493
David Flores <david.floresch45@gmail.com>
2024-01-29 19:47:44 -0500

    ultimo commit

--- index.js
+++ index.js
@@ -1,27 +1,26 @@
+//Inscripción Futbol 7
+//La idea es de crear una plataforma de futbol 7 para que 14 personas se unan para jugar futbol y se encuentren en algun punto de la ciudad.
+//Dichas personas tienen que ser necesariamente mayores de edad y cada equipo esta conformado por 7 personas.
 
 
 function verificarEdad(){
-    
-    const EDAD = parseInt(prompt("Ingresa tu edad"));
-     
-        if(EDAD >= 18){
 
-            let ingresoDeNombre = prompt("Nombre:");
-            console.log("Hola, " + ingresoDeNombre + " .Te esperamos!")
+    let EDAD = prompt("¿Todos son mayores de edad? (responder con si o no)");
+     
+        if((EDAD == "si") || (EDAD == "Si") || (EDAD == "SI")){
+
+            for(let i = 1 ; i <=7; i ++){
             
-            
+            let ingresoDeNombre = prompt("Nombres de tus amiguitos:");
+            console.log("Hola, " + ingresoDeNombre + " .Estas Inscrito. Te esperamos!")
+                
+            }
         }else{
-            console.log("No puede jugar") 
+            console.log("No pueden jugar, por niñitos.")
         }
         
-        let numero = 0;
-            while(numero < 7){
-                verificarEdad();
-                numero ++;
-        
-            }
      }
 
-     verificarEdad();
-
+const resultadoEdad = verificarEdad();
+console.log(resultadoEdad);
 
